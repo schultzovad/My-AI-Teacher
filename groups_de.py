5c685c56678e70fef6d320a1085276d10b344c02
schultzovad <schultzovad@gmail.com>
2026-05-29 21:01:55 +0200

    Delete groups_de.py

--- groups_de.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.set_page_config(layout="wide")
-st.markdown("<style>#MainMenu, footer, header {visibility: hidden;} .stAppDeployButton {display:none;}</style>", unsafe_allow_html=True)
-st.title("👥 Studiengruppen")
-n = st.text_input("Name der neuen Gruppe")
-if st.button("Neue Gruppe erstellen"):
-    st.balloons()
-    st.success(f"Gruppe '{n}' erstellt!")
